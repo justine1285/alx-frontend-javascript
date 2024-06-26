1bd7102864fa48f5bdb01e3eb644cc7a80dfd8fb
justine1285 <justineadugbo@gmail.com>
2024-06-26 10:19:47 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => { console.log('Got a response from the API'); });
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
